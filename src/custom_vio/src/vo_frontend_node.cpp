12088bec447b7df439659e31d3d3e8167825ae8e
YUSUFHASAN02 <yusuf@thekites.ai>
2026-09-18 18:00:35 +0530

    Refactor feature extraction and optical flow functions; add error calculation and trajectory tracking in frontend node

--- src/custom_vio/src/vo_frontend_node.cpp
+++ src/custom_vio/src/vo_frontend_node.cpp
@@ -12,7 +12,7 @@
 #include <vector>
 #include <opencv2/video/tracking.hpp>
 #include <opencv2/features2d.hpp>
-
+#include <opencv2/calib3d.hpp>
 
 using std::placeholders::_1;
 using namespace std::chrono_literals;
@@ -55,9 +55,13 @@ class frontend_node : public rclcpp::Node
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr publisher_;
     size_t count_;
 
+
+
+
+// defining the function to extract the features from the image and return the undistorted image, gray scale image, keypoints and descriptors
     std::tuple<cv::Mat, cv::Mat, std::vector<cv::KeyPoint>, cv::Mat> features_ext(cv::Mat img_frame){
-      cnpy::NpyArray K = cnpy::npy_load(k_path);
-      cnpy::NpyArray D = cnpy::npy_load(d_path);
+      cv :: Mat K = cnpy::npy_load(k_path);
+      cv :: Mat D = cnpy::npy_load(d_path);
       // get the dimentions of the image for undistortion
       cv::Size s = img.size();
       int width = s.width;
@@ -80,20 +84,141 @@ class frontend_node : public rclcpp::Node
       return std::make_tuple(frame, gray_undistorted, keypoints, descriptors);
     }
 
-    void optical_flow(cv::Mat frame_n, cv::mat frame_n_1 , std::vector<cv::KeyPoint> keypoints_n){
 
 
-
-
+// defination of the error function to calculate the error between the forward and backward tracked points
+    std::vector<int> err_call(std::vector<cv::KeyPoint> forwardPts, std::vector<cv::KeyPoint> backPts){
+      
+      std::vector<int> err;
+      std::vector<bool> mask;
+      if (forwardPts.size() != backPts.size()) {
+          std::cerr << "Error: forwardPts and backPts must have the same size." << std::endl;
+          return {};
+      }
+      for (size_t i = 0; i < forwardPts.size(); ++i) {
+          float dx = forwardPts[i].pt.x - backPts[i].pt.x;
+          float dy = forwardPts[i].pt.y - backPts[i].pt.y;
+          err.push_back(std::sqrt(dx * dx + dy * dy));
+      }
+      for (int i = 0 ; i <err.size(); i ++){
+        if (err[i]<1.0){
+          mask.push_back(true);
+        }
+        else{
+          mask.push_back(false);
+        }
+      }
+      return mask;
     }
 
-    void vo_tracker(cv::Mat frame){
+
+  
+// defination of the optical flow function to track the points between the two images
+    std::vector<cv::KeyPoint> optical_flow(cv::Mat prevImg, cv::mat nextImg , std::vector<cv::KeyPoint> prevPts){
+        std::vector<cv::KeyPoint> nextPts;
+        std::vector<std::uchar> status;
+        std::vector<std::float> err;
+        cv::calcOpticalFlowPyrLK(	prevImg, nextImg, prevPts, nextPts,status,err,);
+      
+      return nextPts;
+      }
+
+
+
+// defination of the vo_tracker function to track the points between the two images and calculate the trajectory
+
+    void vo_tracker(cv::Mat prevImg , cv::Mat nextImg){
+      // initializing the arrays to store the trajectories 
+      std::vector<std::float> trajX;
+      std::vector<std::float> trajY;
+      std::vector<std::float> trajZ;
+
+
+      cv :: Mat cameraMatrix = cnpy::npy_load(k_path);
+      cv :: Mat D = cnpy::npy_load(d_path);
+
+
+
+      // extract feature points from the prev frame
+      _, prevGrayUndistorted, prvPts, _ = features_ext(prevImg);
+      if (prvPts.size()<8){
+        // check not to move forward untill enought points are there 
+        std::cout<<"Not enough points in the prev frame"<<endl;
+       return 0; 
+      }
+      else{
+
+        //  cvt next img to gray scale 
+      cv :: Mat nextGrayUndistorted; 
+      cv::cvtColor(nextImg, nextGrayUndistorted, cv::COLOR_BGR2GRAY);
+
+      //  Forward Pass :: tracking of points between the prev frame and the new frame
+
+      nextPts = optical_flow(prevGrayUndistorted,nextGrayUndistorted,prevPts);
+      if (nextPts.size()<8){
+              // check not to move forward untill enought points are there 
+              std::cout<<"Not enough points in the next frame"<<endl;
+            return 0; 
+            }
+
+      // Backward Pass :: tracking of the next points from the new image to the prev image
+      prevPts_back = optical_flow(nextGrayUndistorted,prevGrayUndistorted,nextPts);
+
+      // Calculate the error between the forward and backward tracked points
+
+      std::vector<int> mask = err_call(prevPts,prevPts_back);
+      std::vector<cv::KeyPoint> filteredPrevPts;
+      std::vector<cv::KeyPoint> filteredNextPts;
+
+      // filter the points based on the mask
+      for (int i= 0 ; i < mask.size(); i ++)
+      {
+        if (mask[i]){
+          filteredPrevPts.push_back(prevPts[i]);
+          filteredNextPts.push_back(nextPts[i]);
+        }
+        else{
+          continue;
+        }
+
+      }
+
+      if (filteredPrevPts.size()<8 || filteredNextPts.size()<8)
+      {
+              // check not to move forward untill enought points are there 
+            std::cout<<"Not enough points in the filtered frames"<<endl;
+            return 0; 
+      }
+
+      cv :: Mat essenMatrix =  cv::findEssentialMat	(	filteresPrevPts,filteresPrevPts,cameraMatrix,RANSAC,0.999,1.0,noArray() );
+
+      cv :: Mat R, t;
+
+      cv::recoverPose	(	essenMatrix,
+            filteredPrevPts,
+            filteredNextPts,
+            cameraMatrix,R,t,
+            noArray()
+            )	
+
+      
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+      }
     }
 
     
