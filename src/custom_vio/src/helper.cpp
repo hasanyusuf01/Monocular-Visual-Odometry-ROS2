82dc07a6988f91817783a1d59b918025b3dcf1c3
hasanyusuf01 <yousufhasan1209@gmail.com>
2026-09-22 16:47:42 +0530

    Update .gitignore to include .vscode and add helper.cpp for camera YAML handling

---
+++ src/custom_vio/src/helper.cpp
@@ -0,0 +1,13 @@
+
+
+
+// if cams.empty() {
+//     std::cerr << "No cameras found in the YAML file." << std::endl;
+//     return 1;
+// }
+
+// if (cams.size() > 1) {
+//     std::cerr << "Multiple cameras found in the YAML file. Using the first one." << std::endl;
+// }
+
+// if ()
